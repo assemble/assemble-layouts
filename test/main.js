a097be47c5552bca04a69ca1b48132f6e38eca21
Brian Woodward <brian.woodward@gmail.com>
2014-06-25 17:55:00 -0400

    all the things

---
+++ test/main.js
@@ -0,0 +1,41 @@
+
+var Layouts = require('../');
+var file = require('fs-utils');
+var matter = require('gray-matter');
+
+function loadLayouts (layouts) {
+  file.find('test/fixtures/layouts/*.hbs').forEach(function (filepath) {
+    layouts.set(file.basename(filepath), matter(file.readFileSync(filepath)));
+  });
+}
+
+function loadPages () {
+  return file.find('test/fixtures/pages/*.hbs').map(function (filepath) {
+    return matter(file.readFileSync(filepath));
+  });
+}
+
+describe('Layouts', function () {
+
+  it('should create a layout stack', function () {
+    var layouts = new Layouts({layout: 'default'});
+    loadLayouts(layouts);
+    console.log(layouts);
+    console.log();
+
+    var stack = layouts.createStack({layout: 'default'});
+    console.log('stack', stack);
+    console.log();
+
+    var pages = loadPages();
+    console.log('pages', pages);
+    console.log();
+
+    pages.forEach(function (page) {
+      var template = layouts.render(page);
+      console.log('template', template);
+    });
+
+  });
+
+})
